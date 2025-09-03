6bf58f6f6917fb15c0b03a251858ed2820205229
Mayra Garcia <mayra.alejandra.garcia.garrido@uabc.edu.mx>
2025-09-02 22:09:06 -0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.solitariomagg {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.solitariomagg to javafx.fxml;
+    exports com.example.solitariomagg;
+}
